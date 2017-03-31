8abe772b23fb3f4095a092c612ce3eb8fc75e291
Anna Dmitrieva <annakinkajou@outlook.com>
2017-03-31 22:05:33 +0500

    comment

---
+++ LR8/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LR8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult first()
+        {
+            return View();
+        }
+
+        public IActionResult second()
+        {
+            ViewData["Message"] = "This is second page";
+
+            return View();
+        }
+
+        public IActionResult third()
+        {
+            ViewData["Message"] = "This is third page";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult contResTime()
+
+        {
+            return Content(DateTime.Now.ToString());
+        }
+
+    }
+}
---
+++ LR8/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿getDate();
+function getDate() {
+
+    var query = $.ajax("/Home/Time").done(function (result) {
+
+        $('#date').html(result);
+    });
+}
+
+setInterval(function () {
+    getDate();
+}, 1000);
